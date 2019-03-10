7d3f83d815019486d572266eb238f5a7db0bc612
Felice Forby <feliceforby@gmail.com>
2019-03-10 01:49:29 +0000

    Automatically backed up by Learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -3,9 +3,13 @@ class PostSerializer
     # starting bracket
     serialized_post = '{'
 
-    serialized_post += '"id:" ' + post.id.to_s + ','
-    serialized_post += '"title:" ' + post.title + ','
-    serialized_post += '"description:" ' + post.description + ','
+    serialized_post += '"id": ' + post.id.to_s + ','
+    serialized_post += '"title": ' + post.title + ','
+    serialized_post += '"description": ' + post.description + ','
+
+    # Author association
+    serialized_post += '"author": {'
+    serialized_post += '"name": ' + post.author.name + '"}'
 
     # endig bracket
     serialized_post += '}'
